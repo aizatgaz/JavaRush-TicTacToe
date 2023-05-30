a3c253d04ce4715b79e5f503258faaedca2ab686
Gaz <aizatgaz@mail.ru>
2023-05-30 20:12:58 +0300

    Created 3 classes(InitServlet - for creating a field for game, LogicServlet - is responsible for the tic-tac-toe themselves, and for the outcome of the game(win/draw), RestartServlet - is doing restart game after certain result).
    In main.css was added style for game.
    
    In index.jsp was added jsp code for logic of game and JavaScript for restarting game.

---
+++ src/main/java/com/tictactoe/InitServlet.java
@@ -0,0 +1,35 @@
+package com.tictactoe;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "InitServlet", value = "/start")
+public class InitServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Создание новой сессии
+        HttpSession session = req.getSession(true);
+
+        // Создание игрового поля
+        Field field = new Field();
+
+        // Получение списка значений поля
+        List<Sign> data = field.getFieldData();
+
+        //Добавление в сессию параметров поля (нужно будет для хранения состояния между запросами)
+        session.setAttribute("field", field);
+        // и значений поля, отсортированных по индексу (нужно для отрисовки крестиков и ноликов)
+        session.setAttribute("data", data);
+
+        // Перенаправление запроса на страницу index.jsp через сервер
+        getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
+    }
+
+}
